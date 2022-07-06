229333db6a02b7a225e05602d8a38e0ea2ea42d2
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-07-05 20:59:04 -0700

    all saves buttons all save to local storage

--- script.js
+++ script.js
@@ -48,15 +48,11 @@ function init() {
 }
 
 function savedEvents() {
-    evntNine = localStorage.getItem("eventsAtNine")
-    console.log(evntNine);
-    
-    if (evntNine == null) {
-        console.log(evntNine);
+    var eventThatNine = localStorage.getItem("eventsAtNine")
+    if (eventThatNine == null) {
         document.querySelector("#eventsAtNine").textContent = "..."
     }
-    $("#eventsAtNine").setAttribute.textContent = evntNine;
-    console.log(evntNine);
+    evntNine.textContent = eventThatNine;
 }
 
 
@@ -71,12 +67,36 @@ function hourBar() {
 
 init();
 
-// savedEvents();
+savedEvents();
 
 hourBar();
 
 saveEvent[0].addEventListener("click", function(){
-    console.log("Hello");
-    localStorage.setItem("eventsAtNine", evntNine.textContent);
-    console.log(evntNine.textContent)
-})
+    localStorage.setItem("eventsAtNine", evntNine.textContent);  
+});
+
+saveEvent[1].addEventListener("click", function(){
+    localStorage.setItem("eventsAtTen", evntTen.textContent);
+});
+saveEvent[2].addEventListener("click", function(){
+    localStorage.setItem("eventsAtEleven", evntEleven.textContent);
+});
+saveEvent[3].addEventListener("click", function(){
+    localStorage.setItem("eventsAtTwelve", evntTwelve.textContent);
+});
+saveEvent[4].addEventListener("click", function(){
+    localStorage.setItem("eventsAtOne", evntOne.textContent);
+});
+saveEvent[5].addEventListener("click", function(){
+    localStorage.setItem("eventsAtTwo", evntTwo.textContent);
+});
+saveEvent[6].addEventListener("click", function(){
+    localStorage.setItem("eventsAtThree", evntThree.textContent);
+});
+saveEvent[7].addEventListener("click", function(){
+    localStorage.setItem("eventsAtFour", evntFour.textContent);
+}); 
+saveEvent[8].addEventListener("click", function(){
+    localStorage.setItem("eventsAtFive", evntFive.textContent);
+    console.log(evntFive.textContent)
+});  
